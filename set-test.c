b52c00d310e1eb629f45a26c06ce72534db91146
Alexei A. Smekalkine <ikle@ikle.ru>
2021-11-15 09:54:45 +0300

    set: add simple set test

---
+++ set-test.c
@@ -0,0 +1,50 @@
+/*
+ * Colibri Set Test
+ *
+ * Copyright (c) 2014-2021 Alexei A. Smekalkine
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <stdio.h>
+
+#include <data/set.h>
+#include <data/string.h>
+
+SET_DECLARE (string)
+
+static const char *strings[] = {
+	"test string #1",
+	"Lorem ipsum dolor sit amet",
+	"consectetur adipiscing elit",
+	"sed do eiusmod tempor incididunt",
+	"ut labore et dolore magna aliqua",
+	"test string #2",
+	NULL,
+};
+
+static const char *string_get (const struct string *o)
+{
+	return (void *) o;
+}
+
+int main (int argc, char *argv[])
+{
+	const struct string **list = (void *) strings;
+	struct string_set A;
+	size_t i;
+	const char *entry;
+
+	string_set_init (&A);
+
+	for (i = 0; list[i] != NULL; ++i)
+		if (string_set_add (&A, list[i]) == NULL)
+			fprintf (stderr, "W: cannot add '%s' to set\n",
+				 string_get (list[i]));
+
+	set_foreach (i, entry, &A)
+		printf ("%2zu: %s\n", i, entry);
+
+	string_set_fini (&A);
+	return 0;
+}
