d7bc3239ec923f29ce40459e5d897f2296eee457
Alexei A. Smekalkine <ikle@ikle.ru>
2021-11-15 04:25:50 +0300

    string: restyle a bit, unify style

--- include/data/string.h
+++ include/data/string.h
@@ -11,8 +11,8 @@
 
 #include <data/type.h>
 
-void *string_copy (const void *from);
-int string_eq (const void *o, const void *sample);
+void  *string_copy (const void *from);
+int    string_eq   (const void *o, const void *sample);
 size_t string_hash (size_t iv, const void *o);
 
 const struct data_type string_type;
