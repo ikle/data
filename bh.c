13d403dba0e8c25129de637a19c7838d75efb64e
Alexei A. Smekalkine <ikle@ikle.ru>
2022-09-27 17:14:34 +0300

    bh: restyle and unify code style, no functional changes

--- bh.c
+++ bh.c
@@ -1,7 +1,7 @@
 /*
  * Simple Binary Heap with Wait Queue
  *
- * Copyright (c) 2015,2019 Alexei A. Smekalkine
+ * Copyright (c) 2015-2022 Alexei A. Smekalkine
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -18,131 +18,128 @@
 #define right(i)	(2 * (i) + 2)
 #define parent(i)	(((i) - 1) / 2)
 
-void bh_init (struct bh *bh, int (*cmp) (void *a, void *b))
+void bh_init (struct bh *o, int (*cmp) (void *a, void *b))
 {
-	bh->cmp = cmp;
-	bh->avail = bh->tail = bh->count = 0;
-	bh->pool = NULL;
+	o->cmp   = cmp;
+	o->avail = o->tail = o->count = 0;
+	o->pool  = NULL;
 }
 
-void bh_fini (struct bh *bh, void (*item_free) (void *o))
+void bh_fini (struct bh *o, void (*item_free) (void *o))
 {
 	size_t i;
 
 	if (item_free != NULL)
-		for (i = 0; i < bh->tail; ++i)
-			item_free (bh->pool[i]);
+		for (i = 0; i < o->tail; ++i)
+			item_free (o->pool[i]);
 
-	free (bh->pool);
+	free (o->pool);
 }
 
-void *bh_top (struct bh *bh)
+void *bh_top (struct bh *o)
 {
-	return (bh->count > 0) ? bh->pool[0] : NULL;
+	return (o->count > 0) ? o->pool[0] : NULL;
 }
 
-static int bh_resize (struct bh *bh)
+static int bh_resize (struct bh *o)
 {
 	size_t count;
 	void *pool;
 
-	if (bh->tail < bh->avail)
+	if (o->tail < o->avail)
 		return 1;
 
-	count = (bh->tail + 1) * 3 / 2;
+	count = (o->tail + 1) * 3 / 2;
 
-	pool = realloc (bh->pool, sizeof (bh->pool[0]) * count);
-	if (pool == NULL)
+	if ((pool = realloc (o->pool, sizeof (o->pool[0]) * count)) == NULL)
 		return 0;
 
-	bh->pool = pool;
-	bh->avail = count;
+	o->pool  = pool;
+	o->avail = count;
 	return 1;
 }
 
-static void bh_bubble_up (struct bh *bh, size_t i)
+static void bh_bubble_up (struct bh *o, size_t i)
 {
-	void *x = bh->pool[i];
+	void *x = o->pool[i];
 
-	for (; i > root && bh->cmp (x, bh->pool[parent (i)]); i = parent (i))
-		bh->pool[i] = bh->pool[parent (i)];  /* pull down */
+	for (; i > root && o->cmp (x, o->pool[parent (i)]); i = parent (i))
+		o->pool[i] = o->pool[parent (i)];  /* pull down */
 
-	bh->pool[i] = x;
+	o->pool[i] = x;
 }
 
-static size_t bh_max_child (struct bh *bh, size_t i)
+static size_t bh_max_child (struct bh *o, size_t i)
 {
 	size_t l = left (i), r = right (i);
 
-	if (l >= bh->count)
+	if (l >= o->count)
 		return SIZE_MAX;
 
-	if (r >= bh->count)
+	if (r >= o->count)
 		return l;
 
-	return bh->cmp (bh->pool [r], bh->pool [l]) ? r : l;
+	return o->cmp (o->pool [r], o->pool [l]) ? r : l;
 }
 
-static void bh_bubble_down (struct bh *bh, size_t i)
+static void bh_bubble_down (struct bh *o, size_t i)
 {
-	void *x = bh->pool[i];
+	void *x = o->pool[i];
 	size_t child;
 
 	for (
 		;
-		(child = bh_max_child (bh, i)) < SIZE_MAX &&
-		!bh->cmp (bh->pool [child], x);
+		(child = bh_max_child (o, i)) < SIZE_MAX &&
+		!o->cmp (o->pool [child], x);
 		i = child
 	)
-		bh->pool[i] = bh->pool[child];  /* pull up */
+		o->pool[i] = o->pool[child];  /* pull up */
 
-	bh->pool[i] = x;
+	o->pool[i] = x;
 }
 
-int bh_add (struct bh *bh, void *x)
+int bh_add (struct bh *o, void *x)
 {
-	if (x == NULL || !bh_resize (bh))
+	if (x == NULL || !bh_resize (o))
 		return 0;
 
-	bh->pool[bh->tail++] = x;
-
+	o->pool[o->tail++] = x;
 	return 1;
 }
 
-void bh_commit (struct bh *bh)
+void bh_commit (struct bh *o)
 {
-	for (; bh->count < bh->tail; ++bh->count)
-		bh_bubble_up (bh, bh->count);
+	for (; o->count < o->tail; ++o->count)
+		bh_bubble_up (o, o->count);
 }
 
-int bh_push (struct bh *bh, void *x)
+int bh_push (struct bh *o, void *x)
 {
-	if (!bh_add (bh, x))
+	if (!bh_add (o, x))
 		return 0;
 
-	bh_commit (bh);
-
+	bh_commit (o);
 	return 1;
 }
 
-void *bh_pop (struct bh *bh)
+void *bh_pop (struct bh *o)
 {
 	void *x;
 	void **end;
 
-	if (bh->count == 0)
+	if (o->count == 0)
 		return NULL;
 
-	x = bh->pool[0];
+	x = o->pool[0];
 
-	bh->pool[0] = bh->pool[--bh->count];
-	bh_bubble_down (bh, 0);
+	o->pool[0] = o->pool[--o->count];
+	bh_bubble_down (o, 0);
 
 	/* move queue to rightful place */
-	--bh->tail;
-	end = bh->pool + bh->count;
-	memmove (end, end + 1, (bh->tail - bh->count) * sizeof (*end));
+	--o->tail;
+	end = o->pool + o->count;
+	memmove (end, end + 1, (o->tail - o->count) * sizeof (*end));
 
-	bh_commit (bh);
+	bh_commit (o);
 	return x;
 }
--- include/data/bh.h
+++ include/data/bh.h
@@ -1,7 +1,7 @@
 /*
  * Simple Binary Heap with Wait Queue
  *
- * Copyright (c) 2015-2021 Alexei A. Smekalkine
+ * Copyright (c) 2015-2022 Alexei A. Smekalkine
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -21,27 +21,27 @@ struct bh {
 
 /* initialize heap object for a first use; cmp -- predicate, should return
    nonzero if object a has higher priority over object b */
-void bh_init (struct bh *bh, int (*cmp) (void *a, void *b));
+void bh_init (struct bh *o, int (*cmp) (void *a, void *b));
 
 /* free all internal objects, call destructor (if not NULL) for each
    contained object */
-void bh_fini (struct bh *bh, void (*item_free) (void *o));
+void bh_fini (struct bh *o, void (*item_free) (void *o));
 
 /* returns object with higher priority in head */
-void *bh_top (struct bh *bh);
+void *bh_top (struct bh *o);
 
 /* queue object to heap, but do not insert it immediately, returns nonzero
    on success */
-int bh_add (struct bh *bh, void *x);
+int bh_add (struct bh *o, void *x);
 
 /* commit all pending objects into heap */
-void bh_commit (struct bh *bh);
+void bh_commit (struct bh *o);
 
 /* insert object into heap: queue object to heap and commit all pending
    objects, returns nonzero on success */
-int bh_push (struct bh *bh, void *x);
+int bh_push (struct bh *o, void *x);
 
 /* commit all pending objects and remove top object from heap */
-void *bh_pop (struct bh *bh);
+void *bh_pop (struct bh *o);
 
 #endif  /* COLIBRI_DATA_BH_H */
