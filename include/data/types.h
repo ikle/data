79fe774b19f60b6cbdd72a68a88fd0ce24e59ae8
Alexei A. Smekalkine <ikle@ikle.ru>
2023-09-20 09:35:56 +0300

    Add common types header

---
+++ include/data/types.h
@@ -0,0 +1,27 @@
+/*
+ * Colibri Core Types
+ *
+ * Copyright (c) 2010-2023 Alexei A. Smekalkine
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef COLIBRI_DATA_TYPES_H
+#define COLIBRI_DATA_TYPES_H  1
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/*
+ * container_of (ptr, type, member) -- cast a member of a structure out to
+ * the containing structure, where:
+ *
+ *  ptr    -- the pointer to the member;
+ *  type   -- the type of the container struct this is embedded in;
+ *  member -- the name of the member within the struct.
+ */
+#define container_of(ptr, type, member)  \
+ 	((type *) ((void *) (ptr) - offsetof (type, member)))
+
+#endif  /* COLIBRI_DATA_TYPES_H */
