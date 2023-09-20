a031d875e020f9d5081b437f634c5856755d4a4e
Alexei A. Smekalkine <ikle@ikle.ru>
2023-09-20 04:26:45 +0300

    int-vec: add forgotten method definitions

---
+++ int-vec-copy.c
@@ -0,0 +1,11 @@
+/*
+ * Colibry Integer Vector Copy Constructor
+ *
+ * Copyright (c) 2017-2023 Alexei A. Smekalkine
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <data/int-vec.h>
+
+VEC_DEFINE_COPY (int)
---
+++ int-vec-hash.c
@@ -0,0 +1,11 @@
+/*
+ * Colibry Integer Vector Hash
+ *
+ * Copyright (c) 2017-2023 Alexei A. Smekalkine
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <data/int-vec.h>
+
+VEC_DEFINE_HASH (int)
