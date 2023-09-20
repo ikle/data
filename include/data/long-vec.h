fec47de6360d37fa8b11fe645ef1dbb9786aaf6d
Alexei A. Smekalkine <ikle@ikle.ru>
2023-09-20 03:50:11 +0300

    long: add long vector declaration

---
+++ include/data/long-vec.h
@@ -0,0 +1,17 @@
+/*
+ * Colibry Long Integer Vector Type
+ *
+ * Copyright (c) 2017-2023 Alexei A. Smekalkine
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef COLIBRI_DATA_LONG_VEC_H
+#define COLIBRI_DATA_LONG_VEC_H  1
+
+#include <data/long.h>
+#include <data/vec.h>
+
+VEC_DECLARE_TYPED (long, long, long)
+
+#endif  /* COLIBRI_DATA_LONG_VEC_H */
