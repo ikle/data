7c024629943dd4a532aee5514a9adc0db4dad1f1
Alexei A. Smekalkine <ikle@ikle.ru>
2023-09-20 03:49:47 +0300

    Add long int type methods

---
+++ include/data/long.h
@@ -0,0 +1,16 @@
+/*
+ * Colibry Long Integer Type
+ *
+ * Copyright (c) 2017-2023 Alexei A. Smekalkine
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef COLIBRI_DATA_LONG_H
+#define COLIBRI_DATA_LONG_H  1
+
+#include <data/unit.h>
+
+UNIT_DECLARE_TYPED (long, long, long, sizeof (long) == sizeof (int))
+
+#endif  /* COLIBRI_DATA_LONG_H */
